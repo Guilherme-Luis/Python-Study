799c0a770857f1b68943e8660f9a548045f7a028
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2023-09-13 23:09:15 -0300

    Modificações na biblioteca

--- hello.py
+++ hello.py
@@ -19,15 +19,18 @@ __version__ = "0.0.1"
 __author__ = "Guilherme Luís"
 __license__ = "Unlicensed"
 
-import os
+import locale
 
 
-current_language = os.getenv("LANGUAGE")
-msg = "Hello, World!"
+current_language, _ = locale.getlocale()
+
+msg = "Hello, World" #Default
 
 if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "en_US":
+    msg = "Hello, World!"
 
 print(msg)
